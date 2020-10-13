c710cccf1737cf6826ce76c9e0609ec3f695271f
filmayui <zhongchuyangjie786@gmail.com>
2020-10-13 17:15:39 +0900

    他のユーザーが投稿したプロトタイプの編集ができないように設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,6 @@
 class UsersController < ApplicationController
 
   def show
-    unless user_signed_in?
-      redirect_to root_path
-    end
     @user = User.find(params[:id])
     @prototypes = @user.prototypes.all
   end
